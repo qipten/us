e867f0e79577d80a5d92cc2fc557e7b3dc200a9e
qipten <qipten@gmail.com>
2015-02-24 11:51:34 +0400

    Ini files

---
+++ code.js
@@ -0,0 +1,23 @@
+var window = window;
+var canvas = window.document.getElementById('CVS');
+var context = canvas.getContext('2d');
+
+var game = {
+
+	data: {
+
+	}
+
+	draw: {
+
+	},
+
+	play: {
+
+	},
+
+	update: {
+
+	}
+
+};
